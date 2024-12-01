55d36076b55508225f27e1e896ca7521ba8c1ad5
IkerAllan <Gobe1982>
2024-12-01 15:27:31 -0700

    Adding login services and navbar

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,14 @@
+import {inject, Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  baseUrl = 'https://localhost:5001/api/';
+
+  login(model :any){
+    return this.http.post(`${this.baseUrl}account/login`, model);
+  }
+}
